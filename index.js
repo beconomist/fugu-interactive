25f1433164398f0a19fecf95b871d6b56c8ca6d5
beconomist <beconomist@gmail.com>
2016-05-31 00:37:46 +0800

    change response

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', function(req, res) {
   res.send("Hello World!");
 });
 
-app.listen(process.env.Port || 3000, function() {
-  console.log("Example app listening on port 3000!");
+// app.listen(process.env.Port || 3000, function() {
+//   console.log("Example app listening on port 3000!");
+// });
+
+app.set('port', (process.env.Port || 3000));
+
+app.get('/', function(request, response) {
+    response.send("Hello World!");
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
 });
